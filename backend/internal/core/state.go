839f1bedd88e3402764783121a728b171bcb410d
SAWADA Tadashi <cesare@mayverse.jp>
2026-04-16 10:43:00 +0900

    Add AppState

---
+++ backend/internal/core/state.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type AppState struct {
+	Config Config
+	dbpool *pgxpool.Pool
+}
+
+func CreateAppState(config Config) (*AppState, error) {
+	pool, err := pgxpool.New(context.Background(), config.Database.Url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to establish database pool: %s", err.Error())
+	}
+
+	state := AppState{
+		Config: config,
+		dbpool: pool,
+	}
+	return &state, nil
+}
