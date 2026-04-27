7072343c3fd0c280e74a9559a292500ef3fa0639
SAWADA Tadashi <cesare@mayverse.jp>
2026-04-27 20:21:26 +0900

    Fix problems

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
+// eslint-disable-next-line react-refresh/only-export-components, no-empty-pattern
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
