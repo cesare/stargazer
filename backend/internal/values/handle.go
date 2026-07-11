cfd188e8554d7bffe477ec66c687511a820d7021
SAWADA Tadashi <cesare@mayverse.jp>
2026-07-11 20:33:06 +0900

    Define handle object

---
+++ backend/internal/values/handle.go
@@ -0,0 +1,42 @@
+package values
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+type Handle struct {
+	value string
+}
+
+func TryNewHandleFromUrl(urlString string) (*Handle, error) {
+	parsedUrl, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL %s: %w", urlString, err)
+	}
+
+	pathElements := strings.Split(parsedUrl.Path, "/")
+	handleValue := pathElements[1]
+
+	return TryNewHandle(handleValue)
+}
+
+func TryNewHandle(value string) (*Handle, error) {
+	if !strings.HasPrefix(value, "@") {
+		return nil, fmt.Errorf("handle %s does not starts with @", value)
+	}
+
+	trimmedValue := strings.TrimSpace(value)
+	if len(trimmedValue) < 2 {
+		return nil, fmt.Errorf("handle %s is too short", trimmedValue)
+	}
+
+	return &Handle{
+		value: trimmedValue,
+	}, nil
+}
+
+func (handle *Handle) String() string {
+	return handle.value
+}
